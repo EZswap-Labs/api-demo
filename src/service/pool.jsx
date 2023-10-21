8dc9c0a859e4446f71f38550f5bb14db217fb2e9
zerooooooo1 <xushiduo1@gmail.com>
2023-10-21 21:08:49 +0800

    增加区分环境

--- src/service/pool.jsx
+++ src/service/pool.jsx
@@ -1,9 +1,10 @@
 import axios from 'axios';
+import { BASE_URL } from '../config/constant';
 
 export function queryPoolListByPage(parameter) {
-  // aa
+  console.log('BASE_URL', BASE_URL);
   return axios({
-    url: 'https://testapi.ezswap.io/api/queryCollectionPoolList',
+    url: BASE_URL + 'queryCollectionPoolList',
     method: 'post',
     data: parameter,
   });
